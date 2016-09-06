cb1a9a11ef8ddc31216b2f82aab2020c190fe0cf
Jallvar <kllaid63@yandex.ru>
2016-09-06 20:10:18 +0400

    Add files via upload

--- DataBase.sql
+++ DataBase.sql
@@ -3,7 +3,11 @@
 -- http://www.phpmyadmin.net
 --
 -- Хост: 127.0.0.1
+<<<<<<< HEAD
+-- Время создания: Сен 06 2016 г., 19:58
+=======
 -- Время создания: Сен 05 2016 г., 17:41
+>>>>>>> 871fa622d03607214ff16db11cee729c5b6e03d7
 -- Версия сервера: 5.5.25
 -- Версия PHP: 5.3.13
 
@@ -24,18 +28,41 @@ CREATE TABLE IF NOT EXISTS `authors` (
   `id` int(255) NOT NULL AUTO_INCREMENT,
   `name` varchar(50) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=5 ;
 
 -- --------------------------------------------------------
 
 --
+<<<<<<< HEAD
+-- Структура таблицы `book`
+--
+
+CREATE TABLE IF NOT EXISTS `book` (
+  `id` int(255) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `date` int(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=7 ;
+
+-- --------------------------------------------------------
+
+--
+=======
+>>>>>>> 871fa622d03607214ff16db11cee729c5b6e03d7
 -- Структура таблицы `books`
 --
 
 CREATE TABLE IF NOT EXISTS `books` (
   `id` int(255) NOT NULL AUTO_INCREMENT,
+<<<<<<< HEAD
+  `book` int(255) NOT NULL,
+  `author` int(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=14 ;
+=======
   `name` varchar(255) NOT NULL,
   `author` int(255) NOT NULL,
   `date` int(255) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+>>>>>>> 871fa622d03607214ff16db11cee729c5b6e03d7
